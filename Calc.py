82e1ba0e6bf07d20ab446984a2ed712863e0fcb2
Anish-Mahajan <153806242+Anish-Mahajan@users.noreply.github.com>
2025-04-26 17:56:05 +0530

    Update Calc.py

--- Calc.py
+++ Calc.py
@@ -1,34 +1,8 @@
-# Addition
-num1 = 10
-num2 = 5
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+
 sum_result = num1 + num2
-print("Sum:", sum_result)
 
-# Subtraction
-difference_result = num1 - num2
-print("Difference:", difference_result)
-
-# Multiplication
-product_result = num1 * num2
-print("Product:", product_result)
-
-# Division
-division_result = num1 / num2
-print("Division:", division_result)
-
-# String concatenation
-string1 = "Hello"
-string2 = "World"
-concatenated_string = string1 + " " + string2
-print("Concatenated String:", concatenated_string)
-
-# Conditional statement
-x = 7
-if x > 5:
-    print("x is greater than 5")
-else:
-    print("x is not greater than 5")
-
-# Loop
-for i in range(5):
-    print("Iteration:", i)
+print("The sum is:", sum_result)
