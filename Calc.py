0cf05c0dd37fd60f6ff1c07dcdecff8325cd618c
Anish-Mahajan <153806242+Anish-Mahajan@users.noreply.github.com>
2025-04-26 17:40:27 +0530

    Add files via upload

---
+++ Calc.py
@@ -0,0 +1,34 @@
+# Addition
+num1 = 10
+num2 = 5
+sum_result = num1 + num2
+print("Sum:", sum_result)
+
+# Subtraction
+difference_result = num1 - num2
+print("Difference:", difference_result)
+
+# Multiplication
+product_result = num1 * num2
+print("Product:", product_result)
+
+# Division
+division_result = num1 / num2
+print("Division:", division_result)
+
+# String concatenation
+string1 = "Hello"
+string2 = "World"
+concatenated_string = string1 + " " + string2
+print("Concatenated String:", concatenated_string)
+
+# Conditional statement
+x = 7
+if x > 5:
+    print("x is greater than 5")
+else:
+    print("x is not greater than 5")
+
+# Loop
+for i in range(5):
+    print("Iteration:", i)
